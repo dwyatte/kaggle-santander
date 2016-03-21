b85ce6db7057ab1dca488d57424c6d9ee546378c
dean.wyatte <dean.wyatte@logrhythm.com>
2016-03-21 06:38:08 -0600

    submission w/ best params, vw utils, minor notebook updates

--- santander/utils.py
+++ santander/utils.py
@@ -1,5 +1,7 @@
+import numpy as np
+from copy import deepcopy
 from sklearn.base import BaseEstimator
-
+from sklearn.preprocessing import MinMaxScaler
 
 ZERO_VARIANCE_COLUMNS = [
     'ind_var2_0', 'ind_var2', 'ind_var27_0', 'ind_var28_0', 'ind_var28', 'ind_var27', 'ind_var41', 'ind_var46_0',
@@ -66,5 +68,57 @@ class ColumnDropperCorrelationThreshold(ColumnDropper):
                 if corr.values[i, j] >= self.correlation_threshold and corr.columns[j] not in self.drop:
                     self.drop.append(corr.columns[j])
 
+
+########################################################################################################################
+
+
+def pd_to_vw(filename, features, labels, reweight_importance=False, namespace_to_features=None):
+    """
+    Write pandas features/labels to vw file format
+
+    :param filename: vw output filename
+    :param features: pandas dataframe of features (n x p)
+    :param labels: labels (series or array, n x 1)
+    :param reweight_importance: whether to explicitly reweight importances to deal with class imbalance or leave at 1.0
+    :param namespace_to_features: namespace to feature mapping in format
+    {'namespace1': [feature1 feature2 ...], 'namespace2': [feature1 feature2 ...], ...}. By default, all features are
+    put in a single namespace 'a' for easy quadratic/cubic featurizing
+    :return:
+    """
+    n = features.shape[0]
+
+    if features.shape[0] != labels.shape[-1]:
+        raise ValueError('Features and labels must have same length')
+
+    # reweight importance to deal with class imbalance
+    if reweight_importance:
+        counts = np.bincount(labels)
+        bins = np.nonzero(counts)[0]
+        importance = deepcopy(np.asarray(labels))
+        for b, c in zip(bins, counts):
+            importance[importance == b] = n/float(c)
+    else:
+        importance = np.ones(labels.shape[0])
+
+    # by default, all variables go in one namespace 'a'
+    if namespace_to_features is None:
+        namespace_to_features = {'a': list(features.columns)}
+
+    # labels must be -1/1 for log loss
+    labels = MinMaxScaler((-1, 1)).fit_transform(labels)
+
+    with open(filename, 'w') as f:
+        for i in range(n):
+            line = '%d ' % labels[i]
+            line += '%f ' % importance
+            for namespace in namespace_to_features:
+                line += '|%s ' % namespace
+                d = features.loc[i, namespace_to_features[namespace]].to_dict()
+                line += ' '.join(['%s:%f' % (k, v) for k, v in d.iteritems()])
+            f.write(line+'\n')
+            if i % int(n/10.0) == 0:
+                print '%d/%d' % (i, n)
+
+    print 'Wrote %s' % filename
 
 
--- submission.py
+++ submission.py
@@ -1,25 +1,57 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
+from santander.utils import ColumnDropper
+from santander.utils import ZERO_VARIANCE_COLUMNS, CORRELATED_COLUMNS
+
+pipeline = Pipeline([
+    ('cd', ColumnDropper(drop=ZERO_VARIANCE_COLUMNS+CORRELATED_COLUMNS)),
+])
 
 df_train = pd.read_csv('data/train.csv')
 df_target = df_train['TARGET']
 df_train = df_train.drop(['TARGET', 'ID'], axis=1)
 
+pipeline = pipeline.fit(df_train)
+X_train = pipeline.transform(df_train)
+y_train = df_target
+
 df_test = pd.read_csv('data/test.csv')
 df_id = df_test['ID']
 df_test = df_test.drop(['ID'], axis=1)
+X_test = pipeline.transform(df_test)
+ID_test = df_id
 
-# hand-tuned params for now
-learning_rate = 0.025
-n_estimators = 400
-min_child_weight = 5
-max_depth = 4
+# hand-tuned params
+# learning_rate = 0.025
+# n_estimators = 400
+# min_child_weight = 5
+# max_depth = 4
+
+# better params using columns column subsampling, longer training
+# learning_rate = 0.02
+# n_estimators = 500
+# max_depth = 6
+# subsample = 1
+# colsample_bytree = 0.85
+# min_child_weight = 1  # default
+
+# best params so far using column/row subsampling, even longer training
+learning_rate = 0.01
+n_estimators = 800
+max_depth = 6
+subsample = 0.9
+colsample_bytree = 0.85
+min_child_weight = 1  # default
+
 xgb = XGBClassifier(seed=0, learning_rate=learning_rate, n_estimators=n_estimators,
-                    min_child_weight=min_child_weight, max_depth=max_depth)
-xgb = xgb.fit(df_train, df_target, eval_set=[(df_train, df_target)], eval_metric='auc')
+                    min_child_weight=min_child_weight, max_depth=max_depth,
+                    colsample_bytree=colsample_bytree, subsample=subsample)
+xgb = xgb.fit(X_train, y_train, eval_set=[(X_train, y_train)], eval_metric='auc')
 
-y_pred = xgb.predict_proba(df_test)
-submission = pd.DataFrame({'ID': df_id, 'TARGET': y_pred[:, 1]})
+y_pred = xgb.predict_proba(X_test)
+submission = pd.DataFrame({'ID': ID_test, 'TARGET': y_pred[:, 1]})
 submission.to_csv('submission.csv', index=False)
+print 'Wrote submission.csv'
 
 
