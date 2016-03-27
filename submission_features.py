9758dbf8db82efa03ca859cf5c2bfb99913bc815
dean.wyatte <dean.wyatte@logrhythm.com>
2016-03-27 17:04:37 -0600

    various submissions

--- submission_features.py
+++ submission_features.py
@@ -1,40 +1,36 @@
 import pandas as pd
+
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 from santander.preprocessing import ColumnDropper
-from santander.preprocessing import ZERO_VARIANCE_COLUMNS, CORRELATED_COLUMNS
+from santander.preprocessing import CORRELATED_COLUMNS
+from santander.feature_extraction import BOW, Featurizer
 
 pipeline = Pipeline([
-    ('cd', ColumnDropper(drop=ZERO_VARIANCE_COLUMNS+CORRELATED_COLUMNS)),
+    ('cd', ColumnDropper(drop=CORRELATED_COLUMNS)),
+    ('feat', Featurizer())
 ])
 
 df_train = pd.read_csv('data/train.csv')
 df_target = df_train['TARGET']
 df_train = df_train.drop(['TARGET', 'ID'], axis=1)
 
-pipeline = pipeline.fit(df_train)
-X_train = pipeline.transform(df_train)
-y_train = df_target
-
 df_test = pd.read_csv('data/test.csv')
 df_id = df_test['ID']
 df_test = df_test.drop(['ID'], axis=1)
-X_test = pipeline.transform(df_test)
+
+# combine for a second to run bow/featurizer
+df_all = pd.concat([df_train, df_test], axis=0).reset_index(drop=True)
+num_zero = (df_all == 0).sum(axis=1)
+bow = BOW().fit(df_all).transform(df_all)
+df_all = pd.concat([df_all, bow], axis=1)
+df_all['num_zero'] = num_zero
+df_all = pipeline.fit(df_all).transform(df_all)
+
+X_train = df_all.iloc[:df_train.shape[0], :]
+X_test = df_all.iloc[df_train.shape[0]:, :]
+y_train = df_target
 ID_test = df_id
-
-# hand-tuned params
-# learning_rate = 0.025
-# n_estimators = 400
-# min_child_weight = 5
-# max_depth = 4
-
-# better params using columns column subsampling, longer training
-# learning_rate = 0.02
-# n_estimators = 500
-# max_depth = 6
-# subsample = 1
-# colsample_bytree = 0.85
-# min_child_weight = 1  # default
 
 # best params so far using column/row subsampling, even longer training
 learning_rate = 0.01
@@ -49,9 +45,11 @@ xgb = XGBClassifier(seed=0, learning_rate=learning_rate, n_estimators=n_estimato
                     colsample_bytree=colsample_bytree, subsample=subsample)
 xgb = xgb.fit(X_train, y_train, eval_set=[(X_train, y_train)], eval_metric='auc')
 
+importances = xgb.booster().get_fscore()
+df_importance = pd.DataFrame(zip(importances.keys(), importances.values()), columns=['feature', 'importance'])
+print df_importance.sort_values('importance', ascending=False).reset_index(drop=True)
+
 y_pred = xgb.predict_proba(X_test)
 submission = pd.DataFrame({'ID': ID_test, 'TARGET': y_pred[:, 1]})
-submission.to_csv('submission.csv', index=False)
-print 'Wrote submission.csv'
-
-
+submission.to_csv('submission_features.csv', index=False)
+print 'Wrote submission_features.csv'
