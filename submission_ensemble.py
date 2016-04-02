f790721bc2bac0d1bacd18cf246ac854306e1732
dean.wyatte <dean.wyatte@logrhythm.com>
2016-04-02 09:22:51 -0600

    minor

--- submission_ensemble.py
+++ submission_ensemble.py
@@ -3,11 +3,11 @@ import pandas as pd
 filename = 'submission_ensemble.csv'
 
 # xgb = pd.read_csv('submission_gbm.csv')
-xgb = pd.read_csv('submission_features_gbm_adam.csv')
-keras = pd.read_csv('submission_nn.csv')
+gbm = pd.read_csv('submission_features_gbm_adam.csv')
+nn = pd.read_csv('submission_nn.csv')
 knn_bag = pd.read_csv('submission_knn_bag.csv')
 
-ensemble = xgb.copy()
-ensemble['TARGET'] = xgb['TARGET']*0.8 + keras['TARGET']*0.1 + knn_bag['TARGET']*0.1
+ensemble = gbm.copy()
+ensemble['TARGET'] = gbm['TARGET']*0.8 + nn['TARGET']*0.1 + knn_bag['TARGET']*0.1
 ensemble.to_csv(filename, index=False)
 print 'Wrote %s' % filename
